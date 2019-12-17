2a42c8b9e72f5a920e049fb8b784efb365bb35bc
Miles Higbie <miles.higbie@gmail.com>
2019-12-17 00:10:29 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,38 +1,34 @@
-def join_ingredients(src)
+require "pry"
 
-array = []
-row_index = 0
-while row_index < src.count do 
-      array.push("I love #{src[row_index][0]} and #{src[row_index][1]} on my pizza")
-            row_index +=1 
-              end
-return array
+def join_ingredients(src)
+  src.map do |ingridents|
+    "I love #{ingridents[0]} and #{ingridents[1]} on my pizza"
+  end 
 end
+
+# src = [["pepperoni", "sausage"],
+# ["green olives", "green peppers"],
+# ["onions", "pineapple"]]
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
-  
-  array = []
-  row_index = 0 
-  while row_index < src.length do
-        array.push(src[row_index].max)
-              row_index += 1 
-                end 
-return array
+  src.map do |number_pairs| 
+    number_pairs.max 
+  end 
 end
 
+# src = [[-1, -900], [10, 30], [0, 0], [14, -40.0], [0.8414709848078965, 19]]
+
 def total_even_pairs(src)
- 
- array = []  
- row_index = 0 
- while row_index < src.length do 
-      if src[row_index][0].even? && src[row_index][1].even?
-        array.push(src[row_index][0])
-        array.push(src[row_index][1])
-          end
-             row_index += 1 
-                  end 
-  return array.sum
+total = 0 
+  
 end
+
+# src = [[86262, 58115],aysContainingPairsOfNumbers>)> src
+# [22558, 97709],
+# [66236, 28056],
+# [25992, 81375],
+# [11248, 56789],
+# [42783, 27353],
+# [70796, 63432],
+# [53234, 63058],
+# [72316, 8675309]]
